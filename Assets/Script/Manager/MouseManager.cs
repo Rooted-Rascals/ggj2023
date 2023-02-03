02aba6bd76fb9bc726d54f761df7b429cabb2932
Charles-Antoine <charles.antoine.bellerive.dev@gmail.com>
2023-02-03 17:58:11 -0500

    Fixing zoom

--- Assets/Script/Manager/MouseManager.cs
+++ Assets/Script/Manager/MouseManager.cs
@@ -10,10 +10,12 @@ namespace Script.Manager
     public class MouseManager : MonoBehaviour
     {
         public static MouseManager Instance = null;
-        
+
         private Camera _mainCamera;
         private GameObject _currentGameObject;
+
         private RaycastHit _currentHit;
+
         //Since we are at an angle with a orthographic view, we need to find a "speedratio" so that the speed feels the same.
         private float _speedRatio;
         public UnityEvent<GameObject> onHoverChanged;
@@ -22,6 +24,11 @@ namespace Script.Manager
         [SerializeField] private float cameraSpeed = 1f;
         [SerializeField] private float mouseBorderDetection = 10f;
         [SerializeField] private bool mouseControlEnabled = true;
+        [SerializeField] private float minZoom = 1f;
+        [SerializeField] private float maxZoom = 2f;
+        [SerializeField] private float zoomSpeed = 1f;
+
+        [SerializeField] private GameObject debug;
         public GameObject CurrentSelectedObject { get; private set; }
         public GameObject CurrentHoverObject { get; private set; }
 
@@ -29,11 +36,12 @@ namespace Script.Manager
         {
             if (Instance != null)
                 throw new Exception("Only one MouseManager can be in a scene.");
-            
+
             Instance = this;
-            
+
             _mainCamera = Camera.main;
-            _speedRatio = _mainCamera.transform.localRotation.eulerAngles.x / 90 - _mainCamera.transform.localRotation.eulerAngles.x;
+            _speedRatio = _mainCamera.transform.localRotation.eulerAngles.x / 90 -
+                          _mainCamera.transform.localRotation.eulerAngles.x;
         }
 
         private void Start()
@@ -48,9 +56,10 @@ namespace Script.Manager
             Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
             Physics.Raycast(ray, out _currentHit);
             UpdateSelections(_currentHit.collider?.gameObject);
-            
+
             Move();
             Rotate();
+            Zoom();
         }
 
         void Move()
@@ -58,7 +67,7 @@ namespace Script.Manager
             //Keyboard input
             float xInput = Input.GetAxis("Horizontal");
             float zInput = Input.GetAxis("Vertical");
-            
+
             //Mouse input
             if (mouseControlEnabled && xInput == 0 && zInput == 0)
             {
@@ -71,12 +80,12 @@ namespace Script.Manager
                 else if (Input.mousePosition.y <= 0 + mouseBorderDetection)
                     zInput = -1; //Bottom edge
             }
-            
+
             float xSpeed = xInput * cameraSpeed * _speedRatio;
             float zSpeed = zInput * cameraSpeed * _speedRatio;
-            
-            Vector3 dir = (_mainCamera.transform.right * xSpeed)  + transform.forward * (zSpeed);
-            transform.position += dir * (-1 * Time.deltaTime);    
+
+            Vector3 dir = (_mainCamera.transform.right * xSpeed) + transform.forward * (zSpeed);
+            transform.position += dir * (-1 * Time.deltaTime);
         }
 
         void Rotate()
@@ -84,15 +93,30 @@ namespace Script.Manager
             bool rotateLeft = Input.GetKey(KeyCode.Q);
             bool rotateRight = Input.GetKey(KeyCode.E);
 
-            if (!rotateLeft && !rotateRight) 
+            if (!rotateLeft && !rotateRight)
                 return;
-            
+
             float angle = rotationSpeed;
 
             if (rotateRight)
-                angle = angle * -1;
-                
-            transform.RotateAround(_currentHit.point, Vector3.up, angle);
+                angle *= -1;
+
+            Ray ray = _mainCamera.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0));
+            Physics.Raycast(ray, out RaycastHit middleHit, Mathf.Infinity, LayerMask.GetMask("Ground"));
+            debug.transform.position = middleHit.point;
+            transform.RotateAround(middleHit.point, Vector3.up, angle);
+        }
+
+        private void Zoom()
+        {
+            //TODO : Make this smoother
+            _mainCamera.orthographicSize += Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed;
+            
+            if (_mainCamera.orthographicSize > maxZoom)
+                _mainCamera.orthographicSize = maxZoom;
+            
+            if (_mainCamera.orthographicSize < minZoom)
+                _mainCamera.orthographicSize = minZoom;
         }
 
         private void UpdateSelections(GameObject obj)
