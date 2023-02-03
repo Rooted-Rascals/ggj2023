d4d222f4c164818f16b30d5e84c55713806ae671
Volk <vinxy333@gmail.com>
2023-02-03 16:37:11 -0500

    add different Tiles

--- Assets/HexScript/TilesManager.cs
+++ Assets/HexScript/TilesManager.cs
@@ -9,19 +9,19 @@ public class TilesManager : MonoBehaviour
 
 
     [SerializeField]
-    private GameObject GrassTile;
+    private GameObject Tile;
+
+    [SerializeField]
+    private GameObject TilesParentFolder;
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
 
         for (int i = -gridsize; i <= gridsize; i++)
         {
-            Vector3 SpawnPosition = HexCoordinates.ConvertOffsetToPosition(new Vector3Int(0, i, -i));
-            GameObject t = Instantiate(GrassTile, SpawnPosition, Quaternion.identity);
-            t.GetComponent<GrassTile>().SetPosition(new Vector3Int(0, i, -i));
-            t.name = $"x:{0}_y:{i}_z:{-i}";
+            InstantiateTile(new Vector3Int(0, i, -i));
         }
 
         for (int i = 0; i > -gridsize; i--)
@@ -31,10 +31,7 @@ public class TilesManager : MonoBehaviour
             int z = i;
             while(z <= gridsize)
             {
-                Vector3 SpawnPosition = HexCoordinates.ConvertOffsetToPosition(new Vector3Int(x, y, z));
-                GameObject t = Instantiate(GrassTile, SpawnPosition, Quaternion.identity);
-                t.GetComponent<GrassTile>().SetPosition(new Vector3Int(x, y, z));
-                t.name = $"x:{x}_y:{y}_z:{z}";
+                InstantiateTile(new Vector3Int(x, y, z));
 
                 z++;
                 y--;
@@ -48,16 +45,36 @@ public class TilesManager : MonoBehaviour
             int z = -gridsize;
             while (y >= -gridsize)
             {
-                Vector3 SpawnPosition = HexCoordinates.ConvertOffsetToPosition(new Vector3Int(x, y, z));
-                GameObject t = Instantiate(GrassTile, SpawnPosition, Quaternion.identity);
-                t.GetComponent<GrassTile>().SetPosition(new Vector3Int(x, y, z));
-                t.name = $"x:{x}_y:{y}_z:{z}";
+                InstantiateTile(new Vector3Int(x, y, z));
 
                 z++;
                 y--;
             }
         }
 
+    }
+
+    private void InstantiateTile(Vector3Int position)
+    {
+        Vector3 SpawnPosition = HexCoordinates.ConvertOffsetToPosition(position);
+        GameObject t = Instantiate(Tile, SpawnPosition, Quaternion.identity);
+        t.GetComponent<Tile>().SetPosition(position);
+        t.name = $"x:{position.x}_y:{position.y}_z:{position.z}";
+        t.transform.parent = TilesParentFolder.transform;
+        if(position.x == gridsize || position.x == -gridsize ||
+           position.y == gridsize || position.y == -gridsize ||
+           position.z == gridsize || position.z == -gridsize)
+            t.GetComponent<Tile>().SetTileType(2);
+        else
+        {
+            int xcount = Random.Range(1, 11);
+            if (xcount == 3)
+                t.GetComponent<Tile>().SetTileType(1);
+            else if (xcount == 4)
+                t.GetComponent<Tile>().SetTileType(2);
+            else if (xcount <= 2)
+                t.GetComponent<Tile>().SetTileType(3);
+        }
     }
 
     // Update is called once per frame
