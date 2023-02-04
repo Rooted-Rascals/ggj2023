f5b8879e1392212c46b5ed20e3cb1f2ee7b33b96
Philippe Turner <philippeturner@gmail.com>
2023-02-04 15:19:23 -0500

    [V0] Add plant consumption and generation

--- Assets/Script/Decorators/Plants/Plant.cs
+++ Assets/Script/Decorators/Plants/Plant.cs
@@ -14,6 +14,29 @@ namespace Script.Decorators.Plants
     
     public abstract class Plant : MonoBehaviour
     {
+        [SerializeField] private float waterConsumption = 1f;
+        [SerializeField] private float waterGeneration = 0f;
+        [SerializeField] private float energyGeneration = 0f;
         public abstract PlantType PlantType { get; }
+
+        public virtual void UpdatePlant()
+        {
+            return; // No-Op plant
+        }
+
+        public virtual float GetWaterConsumption()
+        {
+            return waterConsumption;
+        }
+
+        public virtual float GetWaterGeneration()
+        {
+            return waterGeneration;
+        }
+
+        public virtual float GetEnergyGeneration()
+        {
+            return energyGeneration;
+        }
     }
 }
