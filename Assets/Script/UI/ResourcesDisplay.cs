5e7bd2430ac1920ee98e400c9643c56d99ff3784
Volk <vinxy333@gmail.com>
2023-02-05 18:36:03 -0500

    change ui to add healthbar

--- Assets/Script/UI/ResourcesDisplay.cs
+++ Assets/Script/UI/ResourcesDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourcesDisplay : MonoBehaviour
 {
@@ -19,6 +20,12 @@ public class ResourcesDisplay : MonoBehaviour
     [SerializeField] private TextMeshProUGUI timeText;
 
     [SerializeField] private ResourcesManager resourcesManager;
+    [SerializeField] private Image HealthBar;
+
+
+    private HealthManager HealthManager;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,13 +35,21 @@ public class ResourcesDisplay : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        if (HealthManager)
+            HealthBar.fillAmount = Mathf.Lerp(HealthBar.fillAmount, HealthManager.GetHealth() / HealthManager.GetMaxHealth(), 0.1f);
+    }
+
     private void OnGUI()
     {
         energyCountText.text = $"{resourcesManager.GetEnergyCount()}";
         energyProductionText.text = $"+{GameManager.Instance.GetEnergyGeneration()}/s";
         waterCountText.text = $"{resourcesManager.GetWaterCount()}";
-        waterProductionText.text = $"{GameManager.Instance.GetWaterGeneration() - GameManager.Instance.GetWaterConsumption()}/s";
+        float number = GameManager.Instance.GetWaterGeneration() - GameManager.Instance.GetWaterConsumption();
+        waterProductionText.text = (number > 0?"+" + number: number) + "/s";
         healthText.text = $"{GameManager.Instance.GetMotherTree()?.GetComponent<HealthManager>()?.GetHealth() ?? 0}";
         timeText.text = $"{Mathf.RoundToInt(GameManager.Instance.GetTotalTime())}s";
+        HealthManager = GameManager.Instance.GetMotherTree()?.GetComponent<HealthManager>();
     }
 }
