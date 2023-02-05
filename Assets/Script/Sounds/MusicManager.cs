c5c8cd588a8bc9d7c6cd0a7cd609d7226ecf3c4c
Charles-Antoine <charles.antoine.bellerive.dev@gmail.com>
2023-02-04 23:32:18 -0500

    LET THERE BE MUSIC

---
+++ Assets/Script/Sounds/MusicManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Script.Sounds
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class MusicManager : MonoBehaviour
+    {
+        private List<AudioClip> _musics;
+        private AudioSource _source;
+        public void Awake()
+        {
+            _musics = Resources.LoadAll<AudioClip>("Sounds/Music/").ToList();
+            _source = GetComponent<AudioSource>();
+
+        }
+
+        public void Start()
+        {
+            StartCoroutine(nameof(PlayMusic));
+        }
+        
+        IEnumerator PlayMusic()
+        {
+            yield return null;
+
+            while (true)
+            {
+                ShuffleMusic();
+
+                foreach (AudioClip song in _musics)
+                {
+                    _source.clip = song;
+                    _source.Play();
+
+                    while (_source.isPlaying)
+                    {
+                        yield return null;
+                    }
+
+                    yield return new WaitForSeconds(4);
+                }
+            }
+        }
+
+        private void ShuffleMusic()
+        {
+            Random rand = new Random();
+            
+            int n = _musics.Count;  
+            while (n > 1) {  
+                n--;  
+                int k = rand.Next(n + 1);  
+                (_musics[k], _musics[n]) = (_musics[n], _musics[k]);
+            }  
+        }
+    }
+}
